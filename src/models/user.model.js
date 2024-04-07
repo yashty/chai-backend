402cd809ef0245b8870e48eed4d5cfa23518cbc7
Yash <tyagiyash2612@gmail.com>
2024-04-07 22:46:11 +0530

    add cloudinary upload

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,28 +64,30 @@ userSchema.methods.isPasswordCorrect = async function(password){
 }
 
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign({
-        _id:this._id,
-        email:this.email,
-        username: this.username,
-        fullname: this.fullname
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-    }
-)
+    jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 }
 
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign({
-        _id:this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-    }
-)
+    jwt.sign(
+        {
+             _id:this._id,
+        },
+             process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.ECLOUDINARY_CLOUD_NAM,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type:"auto"
+        })
+        // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary", response.url);
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved teemporary file as the uploaded operation got failed
+        return null
+    }
+}
+
+
+export {uploadOnCloudinary}
