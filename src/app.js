ac991d2787fbe219b4b8cf6d1f015b520f0047e5
Yash <tyagiyash2612@gmail.com>
2024-04-10 11:16:41 +0530

    added login and logout functionality

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: false
 }))
 app.use(express.json({limit: "16kb",}))
 app.use(express.urlencoded({extended: true,limit: "16kb"}))
@@ -15,9 +15,8 @@ app.use(cookieParser())
 
 //routes import
 
+// import userRouter from "./routes/user.routes.js"
 import userRouter from "./routes/user.routes.js"
-
-
 //routes declaration
 app.use("api/v1/users", userRouter)
 
